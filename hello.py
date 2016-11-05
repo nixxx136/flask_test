fb4441a8cead531919327faee8d5975fab9f52b3
Penghuan Ni <nixxx136@d.umn.edu>
2016-11-05 10:16:20 -0500

    Add more on TODO list, remove GET method

--- hello.py
+++ hello.py
@@ -5,24 +5,25 @@ import os
 import subprocess
 app = Flask(__name__)
 
-@app.route("/hello2", methods=["POST", "GET"])
+@app.route("/hello2", methods=["POST"])
 def hello2():
-    if request.method == 'POST':
-        obj = request.get_json(silent=True)
-        print(obj)
-        a = obj.get("a", "")
-        b = obj.get("b", "")
-        print("hello2")
-        return (a, b)
-    else:
-        return {"a":2}
+    obj = request.get_json(silent=True)
+    print(obj)
+    a = obj.get("a", "")
+    b = obj.get("b", "")
+
+    # create an json object here and pass that object to ./a.out
+    proc = subprocess.Popen(["./a.out", "data.in"], stdout=subprocess.PIPE)
+    output = proc.communicate()[0]
+    print(output)
+
+    print("hello2")
+    return jsonify(a=a, b=b)
     # return jsonify(a=a, b=b)
 
 @app.route("/")
 def hello(name=None):
     #.get('code', '')
-    proc = subprocess.Popen(["./a.out", "data.in"], stdout=subprocess.PIPE)
-    output = proc.communicate()[0]
     # print(output)
     # os.system('g++ add.cpp')
     # os.system('./a.out data.in')
